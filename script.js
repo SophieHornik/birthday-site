99a92704d6ad7f09191dd46c66c8e389a4bd6f62
Sophie_Hornik Hornik_Sophie <Sophiehornik07@gmail.com>
2024-01-30 13:48:20 -0500

    transferfromreplit

---
+++ script.js
@@ -0,0 +1,59 @@
+let paragraph1 = document.getElementById('paragraph');
+let year = document.getElementById('year');
+let main = document.getElementById('main');
+let pic1 = document.getElementById('pic1');
+let pic2 = document.getElementById('pic2');
+let pic3 = document.getElementById('pic3');
+function show2007() {
+  paragraph.textContent = "On January 10th, 2007, Sophia Frederique Hornik was born at 9:42 pm in New Brunswik hospital. Her first name (Sophia) was after her Grandpa (Saul Hornik). Her middle name (Frederique) was after her papa (Fred Haberman). Sophia’s Older brother, Ethan, decided he did not like the name Sophia so he would only call her “Sophie”, which Sophie has gone by ever since. Sophie is now 17 years old and a student at Marlboro high school."
+  year.textContent = "2007"
+  main.src = "balloons.jpg"
+  pic1.src = "birth.jpg"
+  pic2.src = "preschoolbday.jpg"
+  pic3.src = "collagebday.jpg"
+}
+
+
+let paragraph2 = document.getElementById('paragraph');
+
+function show1776() {
+  paragraph.textContent = "On January 10th, 1776, Thomas Paine's seminal work, Common Sense, was published, sparking a revolutionary wave that would alter the course of American history. Paine's persuasive and straightforward writing style made complex political ideas accessible to the common people, advocating for independence from British rule. His powerful arguments and call for a republic resonated deeply with the American colonists, inspiring widespread support for the revolutionary cause. Common Sense served as a catalyst for the shift in public sentiment, galvanizing the movement for independence and laying the groundwork for the Declaration of Independence later that year. Paine's work not only articulated the grievances against British tyranny but also stirred a sense of unity and purpose among the colonists, ultimately contributing significantly to the birth of the United States of America."
+  year.textContent = "1776"
+  main.src = "thomasp.jpg"
+  pic1.src = "cs.jpg"
+  pic2.src = "csoutline.jpg"
+  pic3.src = "cscrown.jpg"
+
+}
+function show1861() {
+  paragraph.textContent = "On January 10, 1861, Florida officially seceded from the Union, becoming the third state to do so during the tumultuous period leading up to the American Civil War. The state's secession was driven by a combination of economic, social, and political factors, including concerns over the preservation of slavery and states' rights. Florida's decision to join the Confederate States of America intensified the regional tensions and marked a pivotal moment in the escalation toward the Civil War. The impact of Florida's secession was felt not only in the state itself but also across the nation as it fueled the growing divide between the Northern and Southern states. The secession of Florida, along with other Southern states, ultimately culminated in the deadliest conflict in American history, the Civil War, which lasted from 1861 to 1865 and had profound and lasting consequences for the nation."
+  year.textContent = "1861"
+  main.src = "flcw.jpg"
+  pic1.src = "flleaves.jpg"
+  pic2.src = "flmap.jpg"
+  pic3.src = "flflag.png"
+}
+function show1912() {
+  paragraph.textContent = "On January 10th, 1912, Glenn Curtiss achieved a significant milestone in aviation history as his groundbreaking invention, the flying boat airplane, made its maiden flight in Hammondsport, New York. The flying boat was a revolutionary design that featured a hull, enabling it to take off and land on water surfaces. This innovation marked a pivotal moment in aeronautics, as it expanded the possibilities for air travel beyond conventional land-based aircraft. Curtiss's flying boat design became particularly instrumental in maritime operations, reconnaissance, and later, long-distance flights over open water. The success of the first flight at Hammondsport not only showcased Curtiss's engineering prowess but also laid the foundation for the development of seaplanes and flying boats, contributing to the evolution of aviation technology in the early 20th century"
+  year.textContent = "1912"
+  main.src = "boatplane.jpg"
+  pic1.src = "planeone.jpg"
+  pic2.src = "yellowplane.jpg"
+  pic3.src = "glenncurtiss.jpg"
+}
+function show1920() {
+  paragraph.textContent = "On January 10th, 1920, the League of Nations held its inaugural meeting in Paris, marking the formal beginning of this ambitious international organization. Conceived as a response to the devastation of World War I, the League aimed to foster cooperation among nations, promote peace, and prevent future conflicts. Despite its idealistic goals, the League faced challenges in achieving its objectives, primarily due to the absence of powerful nations such as the United States and the Soviet Union. Nevertheless, in the short term, the League facilitated diplomatic efforts and played a role in resolving disputes, promoting disarmament, and addressing issues such as health and labor. Its establishment laid the groundwork for future international cooperation, serving as a precursor to the United Nations formed after World War II. Although the League of Nations ultimately faced limitations and dissolved in the face of the Second World War, its early efforts to promote diplomacy and prevent global conflicts had a lasting impact on the evolution of international relations."
+  year.textContent = "1920"
+  main.src = "LNlogo.png"
+  pic1.src = "peopleln.jpg"
+  pic2.src = "lNnewspaper.jpg"
+  pic3.src = "morepplLN.jpg"
+}
+function show1945() {
+  paragraph.textContent = "Born in London, England, on January 10, 1945, Rod Stewart is a legendary rock and pop singer-songwriter known for his distinctive raspy voice and energetic stage presence. With a career spanning several decades, Stewart has achieved immense success with a string of hit songs such as Maggie May, Do Ya Think I'm Sexy? and Forever Young. His ability to blend rock, folk, and blues elements contributed to his widespread appeal. Rod Stewart has sold over 250 million records worldwide, making him one of the best-selling music artists in history. Beyond his musical achievements, Stewart has been recognized for his charismatic persona, distinctive fashion sense, and dynamic live performances. His impact on the music industry is undeniable, and he remains an enduring figure in the realm of rock and pop music."
+  year.textContent = "1945"
+  main.src = "rsonstage.jpg"
+  pic1.src = "oldrs.jpg"
+  pic2.src = "rsstage2.jpg"
+  pic3.src = "RSchild.jpg"
+}
